1fdbc69222f10998b4b03a4be415fdbd58247405
JoaoJuliasz <joaojuliaszdemorais@gmail.com>
2024-07-14 20:35:41 -0300

    feat: Modal component

---
+++ src/component/Modal/Modal.tsx
@@ -0,0 +1,38 @@
+import { Dispatch, MouseEvent, SetStateAction } from 'react';
+
+import icon from '../../assets/icon/close.svg'
+
+import style from './modal.module.css'
+
+type Props = {
+    headerImg?: string
+    setOpen: Dispatch<SetStateAction<boolean>>
+    children: JSX.Element | JSX.Element[]
+}
+
+const Modal = ({ headerImg, setOpen, children }: Props) => {
+
+    const handleClick = () => setOpen(false)
+
+    const handleOverlayClick = (event: MouseEvent<HTMLDivElement>) => {
+        if (event.target === event.currentTarget) {
+            handleClick();
+        }
+    }
+
+    return (
+        <div className={style.overlay} onClick={handleOverlayClick}>
+            <div className={style.content}>
+                <div className={style.header}>
+                    {headerImg ? <div className={style.bgImg} style={{ backgroundImage: `url(${headerImg})` }} /> : null}
+                    <div onClick={handleClick} className={style.close}>
+                        <img src={icon} alt="Modal close icon" />
+                    </div>
+                </div>
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
